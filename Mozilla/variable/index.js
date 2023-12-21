10377ec829023edcf733e01c75b7940529f4324e
Pousset <62299589+Pousset@users.noreply.github.com>
2023-12-21 09:37:46 +0100

    deccouverte variable

---
+++ Mozilla/variable/index.js
@@ -0,0 +1,13 @@
+// https://developer.mozilla.org/fr/docs/Learn/JavaScript/First_steps/Variables
+
+let myAge = 17;
+let myName = "Mathis";
+let iAmAlive = true;
+let testAge = myAge>18;
+
+let profilArray = [myName, myAge, iAmAlive, testAge];
+
+let cat = {name: "Nina", Age: 4, taille: "70cm"}
+
+let myString = "Hello";
+typeof myString;
