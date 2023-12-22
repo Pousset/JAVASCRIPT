173af53b594bf22cc47850758029c0bceb50722c
Pousset <mathispiette@gmail.com>
2023-12-22 02:08:21 +0100

    tree

---
+++ Mozilla/chaineCaractere/script.js
@@ -0,0 +1,6 @@
+let button = document.querySelector("button")
+
+button.onclick = function () {
+    let name = prompt("Quel est votre nom ?");
+    alert("Hello" + name + "cool de te voir mon pote?");
+}
